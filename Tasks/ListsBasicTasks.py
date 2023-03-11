1681e2235fdc7bb86c4f44a8a01efcc6fa0f5aa7
Aleksey Kibryk <Alexarmy1607@gmail.com>
2023-03-11 19:30:16 +0100

    Add ListsBasicTasks.py full

--- Tasks/ListsBasicTasks.py
+++ Tasks/ListsBasicTasks.py
@@ -19,3 +19,37 @@ def append_sum(my_list):
 print(append_sum([1, 1, 2]))
 
 #Task 3
+def larger_list(my_list1, my_list2):
+  firstLenght = len(my_list1)
+  secondLenght = len(my_list2)
+  if firstLenght >= secondLenght:
+    return my_list1[-1]
+  elif firstLenght < secondLenght:
+    return my_list2[-1]
+
+
+print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10]))
+
+#Task 4
+def larger_list(my_list1, my_list2):
+  if len(my_list1) >= len(my_list2):
+    return my_list1[-1]
+  return my_list2[-1]
+
+print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10]))
+
+#Task 5
+
+def combine_sort(my_list1, my_list2):
+  combined_list = my_list1 + my_list2
+  combined_list.sort()
+  return combined_list
+
+#Task 5.1
+print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10]))
+
+def combined_sort_new(my_list1, my_list2):
+    return sorted(my_list1 + my_list2)
+
+print(combined_sort_new([4, 10, 2, 5], [-10, 2, 5, 10]))
+
