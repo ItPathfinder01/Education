22aa3801dd1aee56ebceb770df782052ea56238f
Aleksey Kibryk <Alexarmy1607@gmail.com>
2023-03-14 22:08:02 +0100

    Add LoopsBasicTasks.py tasks - 4 and 5

--- Tasks/LoopsBasicTasks.py
+++ Tasks/LoopsBasicTasks.py
@@ -29,11 +29,6 @@ print(add_greetings(["Owen", "Max", "Sophie"]))
 
 # Task 3 List comprehension
 
-def add_greetings(names):
-    new_list = ["Hello, " + checker for checker in names]
-    return new_list
-
-
 print(add_greetings(["Owen", "Max", "Sophie"]))
 
 # Solved via regular loop plus append function
@@ -47,13 +42,60 @@ print(add_greetings(["Owen", "Max", "Sophie"]))
 # Task 3
 
 def delete_starting_evens(lst):
- new_list =[]
- for checker in lst:
-     if checker % 2 == 1:
-      new_list = lst.remove(checker)
-     elif checker % 2 == 0:
- return new_list
+    while len(lst) > 0 and lst[0] % 2 == 0:
+        lst = lst[1:]
+    return lst
+
+print(delete_starting_evens([4, 8, 10, 11, 12, 15]))
+print(delete_starting_evens([4, 8, 10]))
+
+# Task 3.1
+
+def delete_starting_evens(lst):
+  index = 0
+  while index < len(lst):
+      element = lst[index]
+      if element % 2 == 0:
+          index += 1
+          continue
+      return lst[index:]
+  return []
 
 
 print(delete_starting_evens([4, 8, 10, 11, 12, 15]))
 print(delete_starting_evens([4, 8, 10]))
+
+# Task 3.2
+
+def delete_starting_evens(lst):
+    for element in lst:
+        if element % 2 == 1:
+            return lst[lst.index(element):]
+    return []
+
+
+print(delete_starting_evens([4, 8, 10, 11, 12, 15]))
+print(delete_starting_evens([4, 8, 10]))
+
+#Task 4
+
+def odd_indices(lst):
+    new_list = [lst[index] for index in range(len(lst)) if index % 2 == 1]
+    return new_list
+
+print(odd_indices([4, 3, 7, 10, 11, -2]))
+
+# Task 5
+
+def exponents(bases, powers):
+  new_list = []
+  for baseElement in bases:
+    for powersElement in powers:
+      new_list.append( baseElement ** powersElement )
+  return new_list
+
+print(exponents([2, 3, 4], [1, 2, 3]))
+
+
+
+
