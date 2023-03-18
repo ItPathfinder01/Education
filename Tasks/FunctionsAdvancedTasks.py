0113e8a4aa7148e9f38de72109095e23f1a3ced4
Aleksey Kibryk <Alexarmy1607@gmail.com>
2023-03-18 21:26:08 +0100

    Add FunctionsAdvancedTasks.py Task 2 and 3

--- Tasks/FunctionsAdvancedTasks.py
+++ Tasks/FunctionsAdvancedTasks.py
@@ -1,11 +1,34 @@
 # Task 1
 
 def first_three_multiples(num):
-  print(num * 1 )
-  print(num * 2 )
-  print(num * 3 )
-  return num * 3
+    print(num * 1)
+    print(num * 2)
+    print(num * 3)
+    return num * 3
+
 
 first_three_multiples(10)
 
 first_three_multiples(0)
+
+
+# Task 2
+
+def tip(total, percentage):
+    tip_amount = (total * percentage) / 100
+    return tip_amount
+
+
+print(tip(10, 25))
+print(tip(0, 100))
+
+
+# Task 3
+def introduction(first_name, last_name):
+    return (last_name + ", " + first_name + " " + last_name)
+
+
+print(introduction("James", "Bond"))
+print(introduction("Maya", "Angelou"))
+
+# Task 4
