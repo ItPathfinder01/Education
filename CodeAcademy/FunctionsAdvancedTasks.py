a9798b7015df35f0ad339af5ec680c957b2b2357
Aleksey Kibryk <Alexarmy1607@gmail.com>
2023-03-20 20:36:02 +0100

    Add FunctionsAdvancedTasks.py and folders refactoring

--- CodeAcademy/FunctionsAdvancedTasks.py
+++ CodeAcademy/FunctionsAdvancedTasks.py
@@ -32,3 +32,33 @@ print(introduction("James", "Bond"))
 print(introduction("Maya", "Angelou"))
 
 # Task 4
+
+def dog_years(name, age):
+  return name + ", " + "you are " + str(age * 7) + " years old in dog years"
+
+
+print(dog_years("Lola", 16))
+
+print(dog_years("Baby", 0))
+
+# Task 5
+
+def lots_of_math(a,b,c,d):
+  ab_sum = a + b
+  cd_minus = c - d
+  prefinal_result = ab_sum * cd_minus
+  fourth = prefinal_result % a
+  print(ab_sum)
+  print(cd_minus)
+  print(prefinal_result)
+  return fourth
+
+
+
+
+print(lots_of_math(1, 2, 3, 4))
+# should print 3, -1, -3, 0
+print(lots_of_math(1, 1, 1, 1))
+# should print 2, 0, 0, 0
+
+
--- CodeAcademy/ListsHomeTasks.py
+++ CodeAcademy/ListsHomeTasks.py
@@ -1,14 +1,15 @@
 # Task 1
 
 def int_numbers():
-    figure = input("Введите свой лист: ")
-    return figure
+    figure_str = input("Введите лист чисел через запятую: ")
+    return figure_str.split(",")
 
 
 def even_numbers():
     figure = int_numbers()
     new_list = [int(element) for element in figure if int(element) % 2 == 0]
 
-    print(new_list)
+    print("Ответ: ", new_list)
 
 even_numbers()
+
