c97b218dcb50f8c708d1d1bec9163fba1fa81ec3
Aleksey Kibryk <Alexarmy1607@gmail.com>
2023-03-22 21:39:38 +0100

    Add FunctionStringSlicing.py Task 2 and 3

--- CodeAcademy/FunctionStringSlicing.py
+++ CodeAcademy/FunctionStringSlicing.py
@@ -9,3 +9,24 @@ def account_generator(first_name, last_name):
   print(new_account)
 
 account_generator(first_name, last_name)
+
+# Task 2
+
+first_name = "Reiko"
+last_name = "Matsuki"
+
+def password_generator(first_name, last_name):
+  temporary_pass = first_name[2:] + last_name[4:]
+  return temporary_pass
+
+temp_password = password_generator(first_name, last_name)
+
+# Task 3
+
+first_name = "Bob"
+last_name = "Daily"
+
+fixed_first_name = "R" + first_name[1:]
+print(fixed_first_name)
+fixed_last_name = "K" + last_name[1:]
+print(fixed_last_name)
