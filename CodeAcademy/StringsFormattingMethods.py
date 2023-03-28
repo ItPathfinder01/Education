fb392d012581f4ff595d57517262320297fa551d
Aleksey Kibryk <Alexarmy1607@gmail.com>
2023-03-29 00:02:13 +0200

    Add StringsFormattingMethods.py Task 7 and 7.1

--- CodeAcademy/StringsFormattingMethods.py
+++ CodeAcademy/StringsFormattingMethods.py
@@ -66,5 +66,41 @@ print (reapers_line_one)
 
 # Task 6
 
+winter_trees_lines = ['All the complicated details', 'of the attiring and', 'the disattiring are completed!', 'A liquid moon', 'moves gently among', 'the long branches.', 'Thus having prepared their buds', 'against a sure winter', 'the wise trees', 'stand sleeping in the cold.']
+
+winter_trees_full = ("\n".join(winter_trees_lines))
+print (winter_trees_full)
+
+# Task 7
+
+def clean_up(*list_to_clean):
+  clean_list = []
+  for cleaner in list_to_clean:
+    for item in cleaner:
+      clean_item = item.strip()
+      clean_list.append(clean_item)
+  return clean_list
+
+def need_to_join(a_list):
+  joined_result = " ".join(a_list)
+  return joined_result
+
+after_clean = clean_up(['Always    ', '     in the middle of our bloodiest battles  ', 'you lay down your arms', '           like flowering mines    ','\n' ,'   to conquer me home.    '])
+
+after_join = need_to_join(after_clean)
+
+print(after_join)
+
+# Task 7.1
+
+love_maybe_lines = ['Always    ', '     in the middle of our bloodiest battles  ', 'you lay down your arms', '           like flowering mines    ','\n' ,'   to conquer me home.    ']
 
 
+love_maybe_lines_stripped = []
+
+for line in love_maybe_lines:
+  love_maybe_lines_stripped.append(line.strip())
+
+love_maybe_full = '\n'.join(love_maybe_lines_stripped)
+
+print(love_maybe_full)
