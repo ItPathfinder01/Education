db7be39438480cb0653a71160d223f060da5de9f
Aleksey Kibryk <Alexarmy1607@gmail.com>
2023-04-01 22:50:16 +0200

    Add StringsFormattingMethods.py Final Task

---
+++ CodeAcademy/StringFormattingTest.py
@@ -0,0 +1,260 @@
+daily_sales = \
+"""Edith Mcbride   ;,;$1.21   ;,;   white ;,; 
+09/15/17   ,Herbert Tran   ;,;   $7.29;,; 
+white&blue;,;   09/15/17 ,Paul Clarke ;,;$12.52 
+;,;   white&blue ;,; 09/15/17 ,Lucille Caldwell   
+;,;   $5.13   ;,; white   ;,; 09/15/17,
+Eduardo George   ;,;$20.39;,; white&yellow 
+;,;09/15/17   ,   Danny Mclaughlin;,;$30.82;,;   
+purple ;,;09/15/17 ,Stacy Vargas;,; $1.85   ;,; 
+purple&yellow ;,;09/15/17,   Shaun Brock;,; 
+$17.98;,;purple&yellow ;,; 09/15/17 , 
+Erick Harper ;,;$17.41;,; blue ;,; 09/15/17, 
+Michelle Howell ;,;$28.59;,; blue;,;   09/15/17   , 
+Carroll Boyd;,; $14.51;,;   purple&blue   ;,;   
+09/15/17   , Teresa Carter   ;,; $19.64 ;,; 
+white;,;09/15/17   ,   Jacob Kennedy ;,; $11.40   
+;,; white&red   ;,; 09/15/17, Craig Chambers;,; 
+$8.79 ;,; white&blue&red   ;,;09/15/17   , Peggy Bell;,; $8.65 ;,;blue   ;,; 09/15/17,   Kenneth Cunningham ;,;   $10.53;,;   green&blue   ;,; 
+09/15/17   ,   Marvin Morgan;,;   $16.49;,; 
+green&blue&red   ;,;   09/15/17 ,Marjorie Russell 
+;,; $6.55 ;,;   green&blue&red;,;   09/15/17 ,
+Israel Cummings;,;   $11.86   ;,;black;,;  
+09/15/17,   June Doyle   ;,;   $22.29 ;,;  
+black&yellow ;,;09/15/17 , Jaime Buchanan   ;,;   
+$8.35;,;   white&black&yellow   ;,;   09/15/17,   
+Rhonda Farmer;,;$2.91 ;,;   white&black&yellow   
+;,;09/15/17, Darren Mckenzie ;,;$22.94;,;green 
+;,;09/15/17,Rufus Malone;,;$4.70   ;,; green&yellow 
+;,; 09/15/17   ,Hubert Miles;,;   $3.59   
+;,;green&yellow&blue;,;   09/15/17   , Joseph Bridges  ;,;$5.66   ;,; green&yellow&purple&blue 
+;,;   09/15/17 , Sergio Murphy   ;,;$17.51   ;,;   
+black   ;,;   09/15/17 , Audrey Ferguson ;,; 
+$5.54;,;black&blue   ;,;09/15/17 ,Edna Williams ;,; 
+$17.13;,; black&blue;,;   09/15/17,   Randy Fleming;,;   $21.13 ;,;black ;,;09/15/17 ,Elisa Hart;,; $0.35   ;,; black&purple;,;   09/15/17   ,
+Ernesto Hunt ;,; $13.91   ;,;   black&purple ;,;   
+09/15/17,   Shannon Chavez   ;,;$19.26   ;,; 
+yellow;,; 09/15/17   , Sammy Cain;,; $5.45;,;   
+yellow&red ;,;09/15/17 ,   Steven Reeves ;,;$5.50   
+;,;   yellow;,;   09/15/17, Ruben Jones   ;,; 
+$14.56 ;,;   yellow&blue;,;09/15/17 , Essie Hansen;,;   $7.33   ;,;   yellow&blue&red
+;,; 09/15/17   ,   Rene Hardy   ;,; $20.22   ;,; 
+black ;,;   09/15/17 ,   Lucy Snyder   ;,; $8.67   
+;,;black&red  ;,; 09/15/17 ,Dallas Obrien ;,;   
+$8.31;,;   black&red ;,;   09/15/17,   Stacey Payne 
+;,;   $15.70   ;,;   white&black&red ;,;09/15/17   
+,   Tanya Cox   ;,;   $6.74   ;,;yellow   ;,; 
+09/15/17 , Melody Moran ;,;   $30.84   
+;,;yellow&black;,;   09/15/17 , Louise Becker   ;,; 
+$12.31 ;,; green&yellow&black;,;   09/15/17 ,
+Ryan Webster;,;$2.94 ;,; yellow ;,; 09/15/17 
+,Justin Blake ;,; $22.46   ;,;white&yellow ;,;   
+09/15/17,   Beverly Baldwin ;,;   $6.60;,;   
+white&yellow&black ;,;09/15/17   ,   Dale Brady   
+;,;   $6.27 ;,; yellow   ;,;09/15/17 ,Guadalupe Potter ;,;$21.12   ;,; yellow;,; 09/15/17   , 
+Desiree Butler ;,;$2.10   ;,;white;,; 09/15/17  
+,Sonja Barnett ;,; $14.22 ;,;white&black;,;   
+09/15/17, Angelica Garza;,;$11.60;,;white&black   
+;,;   09/15/17   ,   Jamie Welch   ;,; $25.27   ;,; 
+white&black&red ;,;09/15/17   ,   Rex Hudson   
+;,;$8.26;,;   purple;,; 09/15/17 ,   Nadine Gibbs 
+;,;   $30.80 ;,;   purple&yellow   ;,; 09/15/17   , 
+Hannah Pratt;,;   $22.61   ;,;   purple&yellow   
+;,;09/15/17,Gayle Richards;,;$22.19 ;,; 
+green&purple&yellow ;,;09/15/17   ,Stanley Holland 
+;,; $7.47   ;,; red ;,; 09/15/17 , Anna Dean;,;$5.49 ;,; yellow&red ;,;   09/15/17   ,
+Terrance Saunders ;,;   $23.70  ;,;green&yellow&red 
+;,; 09/15/17 ,   Brandi Zimmerman ;,; $26.66 ;,; 
+red   ;,;09/15/17 ,Guadalupe Freeman ;,; $25.95;,; 
+green&red ;,;   09/15/17   ,Irving Patterson 
+;,;$19.55 ;,; green&white&red ;,;   09/15/17 ,Karl Ross;,;   $15.68;,;   white ;,;   09/15/17 , Brandy Cortez ;,;$23.57;,;   white&red   ;,;09/15/17, 
+Mamie Riley   ;,;$29.32;,; purple;,;09/15/17 ,Mike Thornton   ;,; $26.44 ;,;   purple   ;,; 09/15/17, 
+Jamie Vaughn   ;,; $17.24;,;green ;,; 09/15/17   , 
+Noah Day ;,;   $8.49   ;,;green   ;,;09/15/17   
+,Josephine Keller ;,;$13.10 ;,;green;,;   09/15/17 ,   Tracey Wolfe;,;$20.39 ;,; red   ;,; 09/15/17 ,
+Ignacio Parks;,;$14.70   ;,; white&red ;,;09/15/17 
+, Beatrice Newman ;,;$22.45   ;,;white&purple&red 
+;,;   09/15/17, Andre Norris   ;,;   $28.46   ;,;   
+red;,;   09/15/17 ,   Albert Lewis ;,; $23.89;,;   
+black&red;,; 09/15/17,   Javier Bailey   ;,;   
+$24.49   ;,; black&red ;,; 09/15/17   , Everett Lyons ;,;$1.81;,;   black&red ;,; 09/15/17 ,   
+Abraham Maxwell;,; $6.81   ;,;green;,;   09/15/17   
+,   Traci Craig ;,;$0.65;,; green&yellow;,; 
+09/15/17 , Jeffrey Jenkins   ;,;$26.45;,; 
+green&yellow&blue   ;,;   09/15/17,   Merle Wilson 
+;,;   $7.69 ;,; purple;,; 09/15/17,Janis Franklin   
+;,;$8.74   ;,; purple&black   ;,;09/15/17 ,  
+Leonard Guerrero ;,;   $1.86   ;,;yellow  
+;,;09/15/17,Lana Sanchez;,;$14.75   ;,; yellow;,;   
+09/15/17   ,Donna Ball ;,; $28.10  ;,; 
+yellow&blue;,;   09/15/17   , Terrell Barber   ;,; 
+$9.91   ;,; green ;,;09/15/17   ,Jody Flores;,; 
+$16.34 ;,; green ;,;   09/15/17,   Daryl Herrera 
+;,;$27.57;,; white;,;   09/15/17   , Miguel Mcguire;,;$5.25;,; white&blue   ;,;   09/15/17 ,   
+Rogelio Gonzalez;,; $9.51;,;   white&black&blue   
+;,;   09/15/17   ,   Lora Hammond ;,;$20.56 ;,; 
+green;,;   09/15/17,Owen Ward;,; $21.64   ;,;   
+green&yellow;,;09/15/17,Malcolm Morales ;,;   
+$24.99   ;,;   green&yellow&black;,; 09/15/17 ,   
+Eric Mcdaniel ;,;$29.70;,; green ;,; 09/15/17 
+,Madeline Estrada;,;   $15.52;,;green;,;   09/15/17 
+, Leticia Manning;,;$15.70 ;,; green&purple;,; 
+09/15/17 ,   Mario Wallace ;,; $12.36 ;,;green ;,; 
+09/15/17,Lewis Glover;,;   $13.66   ;,;   
+green&white;,;09/15/17,   Gail Phelps   ;,;$30.52   
+;,; green&white&blue   ;,; 09/15/17 , Myrtle Morris 
+;,;   $22.66   ;,; green&white&blue;,;09/15/17"""
+
+#------------------------------------------------
+# Start coding below!
+daily_sales_replaced = daily_sales.replace(";,;", "+")
+# print(daily_sales_replaced)
+
+daily_transactions = daily_sales_replaced.split(',')
+# print(daily_transactions)
+
+daily_transactions_split = []
+for pointer in daily_transactions:
+  daily_transactions_split.append(pointer.split("+"))
+# print(daily_transactions_split)
+
+transactions_clean = []
+for brush in daily_transactions_split:
+  transaction_clean = []
+  for small_brush in brush:
+    transaction_clean.append(small_brush.strip())
+  transactions_clean.append(transaction_clean)
+# print(transactions_clean)
+
+customers = []
+sales = []
+thread_sold = []
+for sorter in transactions_clean:
+  customers.append(sorter[0])
+  sales.append(sorter[1])
+  thread_sold.append(sorter[2])
+
+# print (customers)
+# print (sales)
+# print (thread_sold)
+
+total_sales = 0
+for calculate in sales:
+  total_sales += float(calculate.strip("$"))
+
+print("Итоговая сумма за день:",total_sales)
+
+different_color = []
+for unique_color in thread_sold:
+   if unique_color not in  different_color:
+     different_color.append(unique_color)
+
+another_try = ' '.join(different_color)
+# print(another_try)
+another_try1 = another_try.replace("&", " ")
+# print(another_try1)
+another_try2 = another_try1.split(" ")
+# print(another_try2)
+# финальный лист цветов состоящий из синглстрингов
+
+# print(different_color) # общий мультилист и сингл лист
+number_of_colors = []
+for create_single in different_color:
+  if "&" in create_single:
+    number_of_colors.append(create_single.replace("&", ", "))
+# print(number_of_colors)
+# список всех цветов, переведённый в сингл (но ещё листы в листе)
+
+#тут работаю
+
+
+multy_color_split = []
+for delimeter in different_color:
+  if "&" in delimeter:
+    multy_color_split.append(delimeter.split("&"))
+# print(multy_color_split)
+# только мультиколор листы в листе
+
+just_a_list = [] # сингл лист из мульти листа
+for list_maker in multy_color_split:
+  for listik in list_maker:
+    if listik not in just_a_list:
+      just_a_list.append(listik)
+# print(just_a_list)
+# только уникальные синглцвета из мультицвета из листов в листе
+
+single_from_beginning = []
+for delimeter in different_color:
+  if "&" not in delimeter:
+    single_from_beginning.append(delimeter)
+# print(single_from_beginning)
+# лист с изначально сингл цветами
+
+
+intermediate_list = just_a_list + single_from_beginning
+# print(intermediate_list)
+# финальный лист с сингл цветами и сингл цветами из мультицветов)
+thread_sold_split = []
+for colors_sold in intermediate_list:
+  if colors_sold not in thread_sold_split:
+    thread_sold_split.append(colors_sold)
+print("Уникальные цвета, проданные за день:", ", ".join(thread_sold_split))
+
+
+def color_count(color):
+  color_count = 0
+  for color_name in color:
+    color_count += 1
+  return color_count
+
+print("Количество уникальных цветов, проданных сегодня:", color_count(thread_sold_split))
+
+# def the_same_color(color):
+#   same_color_count = 0
+#   for color_name in thread_sold_split:
+#     if color_name == color:
+#       same_color_count += 1
+#   return same_color_count
+
+# print("Число покупок данного цвета из уникального списка, проданного сегодня:", the_same_color("white"))
+
+def the_same_color(color):
+  same_color_count = 0
+  for color_name in another_try2:
+    if color_name == color:
+      same_color_count += 1
+  return same_color_count
+
+print("Число покупок данного цвета, проданного сегодня:", the_same_color("white"))
+
+def the_same_color(red, yellow, green, white, black, blue, purple):
+  number_of_red = 0
+  number_of_yellow = 0
+  number_of_green = 0
+  number_of_white = 0
+  number_of_black = 0
+  number_of_blue = 0
+  number_of_purple = 0
+  for color_name in another_try2:
+    if color_name == "red":
+      number_of_red += 1
+    elif color_name == "yellow":
+      number_of_yellow += 1
+    elif color_name == "green":
+      number_of_green += 1
+    elif color_name == "white":
+      number_of_white += 1
+    elif color_name == "black":
+      number_of_black += 1
+    elif color_name == "blue":
+      number_of_blue += 1
+    elif color_name == "purple":
+      number_of_purple += 1
+  return number_of_red, number_of_yellow, number_of_green, number_of_white, number_of_black, number_of_blue, number_of_purple
+
+
+result = the_same_color("red", "yellow", "green", "white", "black", "blue", "purple")
+print("Количество красных цветов: {}\nКоличество желтых цветов: {}\nКоличество зеленых цветов: {}\nКоличество белых цветов: {}\nКоличество черных цветов: {}\nКоличество синих цветов: {}\nКоличество фиолетовых цветов: {}".format(*result))
+
+
+
--- CodeAcademy/StringsFormattingMethods.py
+++ CodeAcademy/StringsFormattingMethods.py
@@ -1,3 +1,12 @@
+# .upper(), .title(), and .lower() adjust the casing of your string.
+# .split() takes a string and creates a list of substrings.
+# .join() takes a list of strings and creates a string.
+# .strip() cleans off whitespace, or other noise from the beginning and end of a string.
+# .replace() replaces all instances of a character/string in a string with another character/string.
+# .find() searches a string for a character/string and returns the index value that character/string is found at.
+# .format() allows you to interpolate a string with variables.
+
+
 # Task 1
 
 poem_title = "spring storm"
@@ -153,6 +162,37 @@ my_beard_description = poem_description(publishing_date, author, title, original
 print(my_beard_description)
 
 # Task 13
+
+highlighted_poems = "Afterimages:Audre Lorde:1997,  The Shadow:William Carlos Williams:1915, Ecstasy:Gabriela Mistral:1925,   Georgia Dusk:Jean Toomer:1923,   Parting Before Daybreak:An Qi:2014, The Untold Want:Walt Whitman:1871, Mr. Grumpledump's Song:Shel Silverstein:2004, Angel Sound Mexico City:Carmen Boullosa:2013, In Love:Kamala Suraiyya:1965, Dream Variations:Langston Hughes:1994, Dreamwood:Adrienne Rich:1987"
+
+# print(highlighted_poems)
+highlighted_poems_list = highlighted_poems.split(",")
+# print(highlighted_poems_list)
+
+highlighted_poems_stripped = []
+for picker in highlighted_poems_list:
+    highlighted_poems_stripped.append(picker.strip())
+
+print(highlighted_poems_stripped)
+
+highlighted_poems_details = []
+for picker2 in highlighted_poems_stripped:
+  highlighted_poems_details.append(picker2.split(":"))
+
+print(highlighted_poems_details)
+
+titles = []
+poets = []
+dates = []
+for runbig in highlighted_poems_details:
+    titles.append(runbig[0])
+    poets.append(runbig[1])
+    dates.append(runbig[2])
+# print(titles, poets, dates)
+
+for collector in range(0,len(highlighted_poems_details)):
+  print("The poem {} was published by {} in {}.".format(titles[collector],poets[collector],dates[collector] ))
+
 
 
 
