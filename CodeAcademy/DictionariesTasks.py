fb71232b5c5969a91b91da6567c1bde40bb48535
Aleksey Kibryk <Alexarmy1607@gmail.com>
2023-04-04 23:07:28 +0200

    Add DictionariesTasks.py Task 1 and 2

---
+++ CodeAcademy/DictionariesTasks.py
@@ -0,0 +1,14 @@
+# Task 1
+
+sensors =  {"living room": 21, "kitchen": 23, "bedroom": 20, "pantry": 22 }
+num_cameras = {"backyard": 6, "garage": 2, "driveway": 1}
+
+print(sensors)
+print(num_cameras)
+
+# Task 2
+# Creation of the dictionary
+translations = { "mountain": 	"orod", "bread": "bass", "friend": "mellon", "horse": "roch" }
+
+# Task 3
+
