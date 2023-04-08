e47e6b7e04bf1a8d10e6a793c292c5b0f37315ce
Aleksey Kibryk <Alexarmy1607@gmail.com>
2023-04-08 19:37:13 +0200

    Add DictionariesTask.py Task 9

--- CodeAcademy/DictionariesTasks.py
+++ CodeAcademy/DictionariesTasks.py
@@ -1,6 +1,6 @@
 # Task 1
 
-sensors =  {"living room": 21, "kitchen": 23, "bedroom": 20, "pantry": 22 }
+sensors = {"living room": 21, "kitchen": 23, "bedroom": 20, "pantry": 22}
 num_cameras = {"backyard": 6, "garage": 2, "driveway": 1}
 
 print(sensors)
@@ -8,7 +8,7 @@ print(num_cameras)
 
 # Task 2
 # Creation of the dictionary
-translations = { "mountain": "orod", "bread": "bass", "friend": "mellon", "horse": "roch" }
+translations = {"mountain": "orod", "bread": "bass", "friend": "mellon", "horse": "roch"}
 
 # Task 3
 # Adding new stuff or editing xisting
@@ -29,9 +29,10 @@ user_ids.update({"theLooper": 138475, "stringQueen": 85739})
 
 print(user_ids)
 # Task 5
-#Overwriting the dictionary
+# Overwriting the dictionary
 
-oscar_winners = {"Best Picture": "La La Land", "Best Actor": "Casey Affleck", "Best Actress": "Emma Stone", "Animated Feature": "Zootopia"}
+oscar_winners = {"Best Picture": "La La Land", "Best Actor": "Casey Affleck", "Best Actress": "Emma Stone",
+                 "Animated Feature": "Zootopia"}
 
 oscar_winners["Supporting Actress"] = "Viola Davis"
 
@@ -44,7 +45,7 @@ caffeine = [64, 40, 0, 120]
 
 zipped_drinks = zip(drinks, caffeine)
 
-drinks_to_caffeine = {drinks:caffeine for drinks, caffeine in zip(drinks, caffeine)}
+drinks_to_caffeine = {drinks: caffeine for drinks, caffeine in zip(drinks, caffeine)}
 
 print(drinks_to_caffeine)
 
@@ -55,27 +56,30 @@ playcounts = [78, 29, 44, 21, 89, 5]
 
 zipped = zip(songs, playcounts)
 
-plays = {songs: playcounts for songs, playcounts  in zip(songs, playcounts) }
+plays = {songs: playcounts for songs, playcounts in zip(songs, playcounts)}
 
 plays["Purple Haze"] = 1
 plays["Respect"] = 94
 
 # print(plays)
 
-library ={"The Best Songs": plays, "Sunday Feelings": {} }
+library = {"The Best Songs": plays, "Sunday Feelings": {}}
 
 print(library)
 
 # Task 8
 # Getting dictionary value by it's key
-zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": ["Taurus", "Virgo", "Capricorn"], "air":["Gemini", "Libra", "Aquarius"]}
+zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"],
+                   "earth": ["Taurus", "Virgo", "Capricorn"], "air": ["Gemini", "Libra", "Aquarius"]}
 
 print(zodiac_elements["earth"])
 print(zodiac_elements["fire"])
 
+# Task 9
 
+zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"],
+                   "earth": ["Taurus", "Virgo", "Capricorn"], "air": ["Gemini", "Libra", "Aquarius"]}
 
-
-
-
-
+key_to_check = "enegry"
+if key_to_check in zodiac_elements:
+    print(zodiac_elements["energy"])
