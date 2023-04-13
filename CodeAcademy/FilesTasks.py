ce6e6f8f179889f1a8a8e8856d13e4431f4e1466
Aleksey Kibryk <Alexarmy1607@gmail.com>
2023-04-13 17:10:49 +0200

    Add FilesTasks.py 5 and 6

--- CodeAcademy/FilesTasks.py
+++ CodeAcademy/FilesTasks.py
@@ -25,3 +25,19 @@ print(first_line,second_line,third_line)
 with open("bad_bands.txt", "w") as bad_bands_doc:
   bad_bands_doc.write("A Bad Group")
 
+# Task 5
+# Add value to the file (write()) and then read this file (read())
+with open("cool_dogs.txt", "a") as cool_dogs_file:
+  cool_dogs_file.write("Air Buddy \n")
+
+with open("cool_dogs.txt", "r") as cool_dogs_file:
+  text = cool_dogs_file.read()
+
+print(text)
+
+# Task 6
+# CSV parsing
+with open("logger.csv") as log_csv_file:
+  need_to_read = log_csv_file.read()
+print(need_to_read)
+
