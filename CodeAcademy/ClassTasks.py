3b6586fa532715b1bc46512299d7c659ed85f175
Aleksey Kibryk <Alexarmy1607@gmail.com>
2023-04-22 15:54:11 +0200

    Add Filestest.py Task 2

--- CodeAcademy/ClassTasks.py
+++ CodeAcademy/ClassTasks.py
@@ -6,3 +6,8 @@ class Facade:
 facade_1 = Facade()
 facade_1_type = type(facade_1)
 print(facade_1_type)
+
+# Task 2
+class Rules:
+  def washing_brushes(self):
+    return "Point bristles towards the basin while washing your brushes."
