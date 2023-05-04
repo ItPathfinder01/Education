e33096aac1633729689752c3564ddda1d3943aa1
Aleksey Kibryk <Alexarmy1607@gmail.com>
2023-05-04 11:39:41 +0200

    Add DictionariesCodeChallenge.py Task 4

--- CodeAcademy/DictionariesCodeChallenge.py
+++ CodeAcademy/DictionariesCodeChallenge.py
@@ -29,3 +29,28 @@ print(sum_even_keys({10:1, 100:2, 1000:3}))
 # should print 6
 
 # Task 3
+def add_ten(my_dictionary):
+  for key in my_dictionary.keys():
+    my_dictionary[key] += 10
+  return my_dictionary
+
+
+print(add_ten({1: 5, 2: 2, 3: 3}))
+# should print {1:15, 2:12, 3:13}
+print(add_ten({10: 1, 100: 2, 1000: 3}))
+# should print {10:11, 100:12, 1000:13}
+
+# Task 4
+
+def values_that_are_keys(my_dictionary):
+  value_keys = []
+  for value in my_dictionary.values():
+    if value in my_dictionary:
+      value_keys.append(value)
+  return value_keys
+
+
+print(values_that_are_keys({1:100, 2:1, 3:4, 4:10}))
+# should print [1, 4]
+print(values_that_are_keys({"a":"apple", "b":"a", "c":100}))
+# should print ["a"]
