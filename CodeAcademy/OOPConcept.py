4bd33338ca9282826c0e7ca8d0b9df8a4cd5595b
Aleksey Kibryk <Alexarmy1607@gmail.com>
2023-05-08 10:19:44 +0200

    Add OOPConcept.py Task 1 (Super method)

--- CodeAcademy/OOPConcept.py
+++ CodeAcademy/OOPConcept.py
@@ -9,12 +9,18 @@ class Employee:
     def say_id(self):
         print(f"My id is {self.id}")
 
+class Admin(Employee): # Inherited Employee method
+  def say_id(self):    # Override the method with another output
+    super().say_id()   # Super method allows to use the logic from Employee class
+    print("I am an Admin")
+
 e1 = Employee()
 e2 = Employee()
 e3 = Employee()
+e4 = Admin()
 
 e1.say_id()
 e2.say_id()
 e3.say_id()
-
+e4.say_id()
 # Task 2
