b3b49a3a37e795ca2e71ff1bf5fcb7ca9424ae02
Aleksey Kibryk <Alexarmy1607@gmail.com>
2023-05-20 12:47:32 +0200

    Add FunctionArguments.py Task 1

---
+++ CodeAcademy/FunctionArguments.py
@@ -0,0 +1,21 @@
+# Task 1 Positional, Keyword and Default arguments
+
+tables = {
+  1: ['Jiho', False],
+  2: [],
+  3: [],
+  4: [],
+  5: [],
+  6: [],
+  7: [],
+}
+print(tables)
+
+def assign_table(table_number, name, vip_status = False):
+  tables.update({table_number:[name, vip_status]})
+
+
+assign_table(6, "Yoni", False)
+assign_table(table_number = 3, name = "Martha", vip_status = True)
+assign_table(4, "Karla")
+print(tables)
