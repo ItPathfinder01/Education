37c35bcc5c774d834474569b6df502284dfbddf3
Aleksey Kibryk <Alexarmy1607@gmail.com>
2023-05-21 10:12:59 +0200

    Add FunctionArguments.py Task 5 working with *args

--- CodeAcademy/FunctionArguments.py
+++ CodeAcademy/FunctionArguments.py
@@ -61,4 +61,73 @@ assign_and_print_order(2, 'Steak', 'Seabass', 'Wine Bottle')
 
 print(tables)
 
+# Task 4 **kwargs
+
+tables = {
+  1: {
+    'name': 'Chioma',
+    'vip_status': False,
+    'order': {
+      'drinks': 'Orange Juice, Apple Juice',
+      'food_items': 'Pancakes'
+    }
+  },
+  2: {},
+  3: {},
+  4: {},
+  5: {},
+  6: {},
+  7: {},
+}
+print(tables)
+
+
+def assign_food_items(**order_items):
+  print(order_items)
+  food = order_items.get("food")
+  drinks = order_items.get("drinks")
+  print(food)
+  print(drinks)
+
+
+
+assign_food_items(food='Pancakes, Poached Egg', drinks='Water')
+
+# Task 5
+
+tables = {
+  1: {
+    'name': 'Chioma',
+    'vip_status': False,
+    'order': {
+      'drinks': 'Orange Juice, Apple Juice',
+      'food_items': 'Pancakes'
+    }
+  },
+  2: {},
+  3: {},
+  4: {},
+  5: {},
+  6: {},
+  7: {},
+}
+
+def assign_table(table_number, name, vip_status=False):
+  tables[table_number]['name'] = name
+  tables[table_number]['vip_status'] = vip_status
+  tables[table_number]['order'] = {}
+
+assign_table(2, 'Douglas', True)
+print('--- tables with Douglas --- \n', tables)
+
+def assign_food_items(table_number,**order_items):
+  food = order_items.get('food')
+  drinks = order_items.get('drinks')
+  tables[table_number]['order']['food_items'] = food
+  tables[table_number]['order']['drinks'] = drinks
+
+print('\n --- tables after update --- \n')
+assign_food_items(2, food ='Seabass, Gnocchi, Pizza', drinks = 'Margarita, Water' )
+print(tables)
+
 
